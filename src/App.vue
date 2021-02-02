837a43318a31e165aaeb1ccbb68218dcf5c32de2
Miika Alikirri <miika.alik@gmail.com>
2021-02-02 13:14:32 +0200

    Initial light and dark themes

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,30 @@
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
+            <v-icon color="secondary">mdi-view-dashboard</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title class="primary--text">Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-cog</v-icon>
+            <v-icon color="secondary">mdi-cog</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Data View</v-list-item-title>
+            <v-list-item-title class="primary--text">
+              Data View
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-api</v-icon>
+            <v-icon color="secondary">mdi-api</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Api View</v-list-item-title>
+            <v-list-item-title class="primary--text">
+              Api View
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -31,7 +35,15 @@
 
     <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title class="primary--text">Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn large icon dark>
+          <v-icon @click="changeTheme()" size="30" color="primary">
+            {{ lightIcon }}
+          </v-icon>
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-main>
@@ -50,14 +62,22 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: "App",
+@Component
+export default class App extends Vue {
+  public drawer = null;
+  public lightIcon = "mdi-lightbulb-on-outline";
 
-  components: {},
+  changeTheme() {
+    const dark = !this.$vuetify.theme.dark;
+    this.$vuetify.theme.dark = dark;
+    this.lightIcon = dark ? "mdi-lightbulb " : "mdi-lightbulb-on-outline";
+  }
 
-  data: () => ({
-    drawer: null
-  })
-});
+  // light on
+  // mdi-lightbulb-on-outline
+  // light off
+  // mdi-lightbulb
+}
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,6 +5,26 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-    dark: true
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#71b5ff",
+        accent: "#afd7ff",
+        secondary: "#517397",
+        success: "#82a0c4",
+        info: "#638caa",
+        warning: "#5c6bc0",
+        error: "#529cff"
+      },
+      light: {
+        primary: "#38587b",
+        accent: "#161f28",
+        secondary: "#517397",
+        success: "#82a0c4",
+        info: "#638caa",
+        warning: "#5c6bc0",
+        error: "#529cff"
+      }
+    }
   }
 });
