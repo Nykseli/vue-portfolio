b06dc1649e58a1e3fe97443fef7dc3a24fc69105
Miika Alikirri <miika.alik@gmail.com>
2021-02-17 19:36:40 +0200

    Creted GitInfo component out of Exprience
    Now the same layout can be easily reused between
    all the different views

---
+++ src/components/GifInfo.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container class="gifinfo-container" fill-height fluid>
+    <v-row align="center" justify="center">
+      <h1 class="success--text gifinfo-main-header">{{ title }}</h1>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col align="center" sm="6">
+        <v-img class="animated-gif" :src="gifPath" />
+      </v-col>
+      <v-col class="gifinfo-text primary--text" align="center" sm="6">
+        <v-card class="gifinfo-scroll-card">
+          <!-- The layout is injected to replace to slot -->
+          <slot></slot>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class GifInfo extends Vue {
+  @Prop({ required: true }) private title!: string;
+  @Prop({ required: true }) private gifPath!: string;
+}
+</script>
+
+<style scoped>
+.gifinfo-container {
+  width: 100vw;
+  max-width: 100vw;
+  height: 100vh;
+  max-height: 100vh;
+  overflow-x: hidden;
+}
+
+.gifinfo-text {
+  text-align: left;
+  padding-bottom: 20px;
+}
+
+.gifinfo-scroll-card {
+  height: 70vh;
+  box-shadow: none !important;
+}
+
+.gifinfo-main-header {
+  font-size: 54px;
+  padding-top: 50px;
+}
+
+/* 600px is the sm grid brekpoint */
+@media (min-width: 600px) {
+  .gifinfo-scroll-card {
+    overflow-y: scroll !important;
+  }
+
+  .gifinfo-text {
+    padding-left: 2.5%;
+    padding-right: 2.5%;
+  }
+
+  .gifinfo-main-header {
+    font-size: 54px;
+    padding-top: 50px;
+    padding-bottom: 0px;
+  }
+}
+
+@media (max-width: 599px) {
+  .gifinfo-scroll-card {
+    overflow-y: visible !important;
+  }
+
+  .gifinfo-text {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
+
+  .gifinfo-main-header {
+    font-size: 40px;
+    padding-top: 75px;
+    padding-bottom: 25px;
+  }
+}
+
+.animated-gif {
+  width: 60% !important;
+  height: 60% !important;
+}
+</style>
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,43 +1,29 @@
 <template>
-  <v-container class="experience-container" fill-height fluid>
-    <v-row align="center" justify="center">
-      <h1 class="success--text main-header">Work experience</h1>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col align="center" sm="6">
-        <v-img class="animated-gif" src="images/music-WIP1.gif" />
-      </v-col>
-      <v-col class="experience-text primary--text" align="center" sm="6">
-        <v-card class="info-scroll-card">
-          <div
-            class="info-job-item"
-            v-for="(item, index) in items"
-            :key="index"
-          >
-            <h1 class="accent--text">{{ item.title }}</h1>
-            <h2 class="primary--text">{{ item.company }}</h2>
-            <h3 class="primary--text">{{ item.duration }}</h3>
-            <p
-              class="secondary--text"
-              v-for="(p, i2) in item.descriptions"
-              :key="i2"
-            >
-              - {{ p }}
-            </p>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <gif-info :title="title" gifPath="images/music-WIP1.gif">
+    <div class="info-job-item" v-for="(item, index) in items" :key="index">
+      <h1 class="accent--text">{{ item.title }}</h1>
+      <h2 class="primary--text">{{ item.company }}</h2>
+      <h3 class="primary--text">{{ item.duration }}</h3>
+      <p class="secondary--text" v-for="(p, i2) in item.descriptions" :key="i2">
+        - {{ p }}
+      </p>
+    </div>
+  </gif-info>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import GifInfo from "@/components/GifInfo.vue";
 
-@Component({})
+@Component({
+  components: {
+    GifInfo
+  }
+})
 export default class Experience extends Vue {
   // TODO: i18n
   // TODO: actual text
+  public title = "Work experience";
   public items = [
     {
       title: "Job title",
@@ -74,31 +60,8 @@ export default class Experience extends Vue {
 </script>
 
 <style scoped>
-.experience-container {
-  width: 100vw;
-  max-width: 100vw;
-  height: 100vh;
-  max-height: 100vh;
-  overflow-x: hidden;
-}
-
-.experience-title {
-  display: inline-block;
-  text-align: right;
-}
-
-.vuebar-element {
-  max-height: 70vh;
-}
-
-.experience-text {
-  text-align: left;
-  padding-bottom: 20px;
-}
-
-.info-scroll-card {
-  height: 70vh;
-  box-shadow: none !important;
+.info-job-item {
+  margin-right: 10px;
 }
 
 .info-job-item > h1 {
@@ -112,50 +75,5 @@ export default class Experience extends Vue {
 .info-job-item > h3 {
   font-size: 18px;
   padding-bottom: 10px;
-}
-
-.main-header {
-  font-size: 54px;
-  padding-top: 50px;
-}
-
-/* 600px is the sm grid brekpoint */
-@media (min-width: 600px) {
-  .info-scroll-card {
-    overflow-y: scroll !important;
-  }
-
-  .experience-text {
-    padding-left: 2.5%;
-    padding-right: 2.5%;
-  }
-
-  .main-header {
-    font-size: 54px;
-    padding-top: 50px;
-    padding-bottom: 0px;
-  }
-}
-
-@media (max-width: 599px) {
-  .info-scroll-card {
-    overflow-y: visible !important;
-  }
-
-  .experience-text {
-    padding-left: 5%;
-    padding-right: 5%;
-  }
-
-  .main-header {
-    font-size: 40px;
-    padding-top: 75px;
-    padding-bottom: 25px;
-  }
-}
-
-.animated-gif {
-  width: 60% !important;
-  height: 60% !important;
 }
 </style>
