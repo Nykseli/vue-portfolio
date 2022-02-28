635ee25563ff06704956586e35fe19d55c110bc8
Miika Alikirri <miika.alik@gmail.com>
2022-02-28 20:15:34 +0200

    Deploy the website!

---
+++ deploy.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+###
+# Script for copying files to the target server with rsync
+##
+
+###
+# Build and deploy the website.
+# This assumes that node_modules are installed
+##
+
+###
+# Env variables
+##
+TARGET_USER=$WDEPLOY_USER
+TARGET_PASS=$WDEPLOY_PASS
+TARGET_URL=$WDEPLOY_URL
+TARGET_PATH=$WDEPLOY_PATH
+
+set -e
+
+function usage {
+    echo "Usage bash deploy.sh normal|nobuild"
+    echo "normal:"
+    echo "      build and deploy"
+    echo "nobuild:"
+    echo "      no build. only deploy"
+    exit
+}
+
+# if no argument
+if [[ $# -eq 0 ]]
+then
+    usage
+fi
+
+
+function transfer_files {
+    TARGET_RSYNC="scp -r dist/. $TARGET_USER@$TARGET_URL:$TARGET_PATH"
+
+    echo "Transfering files."
+    $TARGET_RSYNC
+    echo "Transfer done."
+}
+
+function normal_transfer {
+    npm run build
+    transfer_files
+}
+
+function nobuild_transfer {
+    transfer_files
+}
+
+if [ -z "$TARGET_USER" ]; then
+    echo "Couldn't find the WDEPLOY_USER env variable"
+    exit 1
+fi
+
+if [ -z "$TARGET_PASS" ]; then
+    echo "Couldn't find the WDEPLOY_PASS env variable"
+    exit 1
+fi
+
+if [ -z "$TARGET_URL" ]; then
+    echo "Couldn't find the WDEPLOYT_URL env variable"
+    exit 1
+fi
+
+if [ -z "$TARGET_PATH" ]; then
+    echo "Couldn't find the WDEPLOY_PATH env variable"
+    exit 1
+fi
+
+UPDATE_TARGET=$1
+
+# Set the UPDATE_FILES based on the argument
+case "$UPDATE_TARGET"
+in
+    "normal")
+        normal_transfer
+        ;;
+    "nobuild")
+        nobuild_transfer
+        ;;
+    *)
+        usage
+        ;;
+esac
